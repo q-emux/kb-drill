f2b509df05f190980bf7d5b13e25734cd5365bb5
q-emux <elacara@gmail.com>
2026-09-15 14:57:23 +0200

    N-ma richiesta modifica

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kb-drill-20260915-1244.fbf866e';
+const CACHE = 'kb-drill-20260915-1251.fbf866e';
 const ASSETS = ['./', './index.html', './kb.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
