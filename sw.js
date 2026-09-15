b94dab28f37ba80d6e25dcc1300c884a2d5d30ed
q-emux <elacara@gmail.com>
2026-09-15 14:25:28 +0200

    correzioni card

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kb-drill-20260915-1154.fbf866e';
+const CACHE = 'kb-drill-20260915-1223.fbf866e';
 const ASSETS = ['./', './index.html', './kb.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
