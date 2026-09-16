0797284c62d23cf3c432d3db17ab72251f769e50
q-emux <elacara@gmail.com>
2026-09-16 11:33:44 +0200

    altri bug fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kb-drill-20260916-0851.2bba3f5';
+const CACHE = 'kb-drill-20260916-0924.6655012';
 const ASSETS = ['./', './index.html', './kb.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
