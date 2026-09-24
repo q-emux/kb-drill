39f4b7e18f281934723714092ef28784f6c2bb21
q-emux <elacara@gmail.com>
2026-09-24 18:13:28 +0200

    pdate

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kb-drill-20260924-1516.926f3ec';
+const CACHE = 'kb-drill-20260924-1605.0bc0b49';
 const ASSETS = ['./', './index.html', './kb.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
